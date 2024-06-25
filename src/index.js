945fbfc4587d24e912b34d240cede2d646e3ef75
tomcat <53894241+tomcat402@users.noreply.github.com>
2024-06-25 13:04:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.tg1.co.za": dockerHub,
+  "quay.tg1.co.za": "https://quay.io",
+  "gcr.tg1.co.za": "https://gcr.io",
+  "k8s-gcr.tg1.co.za": "https://k8s.gcr.io",
+  "k8s.tg1.co.za": "https://registry.k8s.io",
+  "ghcr.tg1.co.za": "https://ghcr.io",
+  "cloudsmith.tg1.co.za": "https://docker.cloudsmith.io",
+  "ecr.tg1.co.za": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.tg1.co.za": dockerHub,
 };
 
 function routeByHosts(host) {
